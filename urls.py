8e90e0eea7e637d53f74a6de646ebb4d670bb49e
piotrek <wasilewski.piotrek@gmail.com>
2011-07-12 06:11:13 +0200

    Updated main URLs to work with moved apps

--- urls.py
+++ urls.py
@@ -7,19 +7,24 @@ admin.autodiscover()
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
-    (r'^api/', include('webapi.urls')),
-    (r'^network/', include('networks.urls')),
-    (r'^event/', include('events.urls')),
-    (r'^report/', include('reportmeta.urls')),
+    (r'^api/', include('netadmin.webapi.urls')),
+    (r'^network/', include('netadmin.networks.urls')),
+    (r'^event/', include('netadmin.events.urls')),
+    (r'^report/', include('netadmin.reportmeta.urls')),
+    (r'^user/', include('netadmin.users.urls')),
+    (r'^notifier/', include('netadmin.notifier.urls')),
     
-    ('^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
+    ('^$', 'django.views.generic.simple.direct_to_template',
+     {'template': 'home.html'}),
     (r'^accounts/', include(admin.site.urls)),
     
-    url(r'login/', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    url(r'login/', 'django.contrib.auth.views.login',
+        {'template_name': 'login.html'}, name='login_page'),
 
     url(r'^oauth/request_token/$','piston.authentication.oauth_request_token'),
     url(r'^oauth/authorize/$','piston.authentication.oauth_user_auth'),
     url(r'^oauth/access_token/$','piston.authentication.oauth_access_token'),
     
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+     {'document_root': settings.MEDIA_ROOT}),
 )
