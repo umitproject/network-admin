eeab2509a40989898724384ba7583db919c27eea
piotrek <wasilewski.piotrek@gmail.com>
2011-07-31 00:34:14 +0200

    Modified alerts inclusion tag, created events list inclusion tag, created templates: events search, events types edit and events stats, Created similar events inclusion tag

--- netadmin/events/templatetags/events_tags.py
+++ netadmin/events/templatetags/events_tags.py
@@ -18,9 +18,13 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from django import template
+import datetime
 
-from netadmin.events.models import Alert, ALERT_LEVELS
+from django import template
+from django.utils.translation import ugettext as _
+
+from netadmin.events.models import Event, EventType, ALERT_LEVELS
+from netadmin.events.utils import filter_user_events
 
 
 register = template.Library()
@@ -28,12 +32,35 @@ register = template.Library()
 
 @register.inclusion_tag('events/alerts_counter.html')
 def alerts_counter(user_id):
-    user_alerts = Alert.objects.filter(user__pk=user_id)
-    alert_levels = []
+    user_alerts = EventType.objects.filter(user__pk=user_id,
+                                           alert_level__gte=1)
+    
+    alert_levels = {}
+    for alert in user_alerts:
+        level = alert.alert_level
+        if level in alert_levels:
+            alert_levels[level] += alert.events.count()
+        else:
+            alert_levels[level] = alert.events.count()
+    
+    alert_levels_list = []
     for id, name in ALERT_LEVELS:
-        alerts = user_alerts.filter(level=id)
-        count = 0
-        for alert in alerts:
-            count += alert.event_type.events.count()
-        alert_levels.append((id, name, count))
-    return {'alert_levels': alert_levels}
+        if id in alert_levels:
+            count = alert_levels[id]
+            alert_levels_list.append((id, name, count))
+    return {'alert_levels': alert_levels_list}
+
+@register.inclusion_tag('events/events_list_tag.html')
+def events_list(events, title=None):
+    context = {
+        'events': events,
+        'title': title
+    }
+    return context
+
+@register.inclusion_tag('events/events_list_tag.html')
+def similar_events(event):
+    event_type = event.event_type
+    events = filter_user_events(event.user).filter(event_type=event_type)
+    events = events.exclude(pk=event.pk)
+    return events_list(events[:10], "Similar events")
