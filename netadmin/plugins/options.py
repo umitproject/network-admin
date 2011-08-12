a584627c0e532d9d80c15ab0697559ba66d94afe
piotrek <wasilewski.piotrek@gmail.com>
2011-08-12 02:36:36 +0200

    Updated docstrings for options. Setting and getting global/user options

--- netadmin/plugins/options.py
+++ netadmin/plugins/options.py
@@ -18,9 +18,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from django import forms
 from django.utils.translation import ugettext as _
 
-from models import CustomOption
+from models import CustomOption, CUSTOM_OPTION_MAX_LENGTH
 
 
 class UnknownOption(Exception):
@@ -34,34 +35,94 @@ class OptionDuplicated(Exception):
     pass
 
 
-def set_options(name, value):
+def set_option(name, value, user=None):
     """Sets value of an option
     """
-    option, created = CustomOption.objects.get_or_create(name=name)
+    option, created = CustomOption.objects.get_or_create(name=name, user=user)
     option.value = str(value)
     option.save()
     return option
 
-def reset_option(name, value):
+def reset_option(name, value, user=None):
     """
     Removes all definitions of an option and sets its value once again.
     
-    Use this function in case of OptionDuplicated error.
+    Use this function in case of OptionDuplicated error, e.g.
+    
+        # get value of a global option and make sure it's not duplicated
+        try:
+            value = get_option(option_name)
+        except OptionDuplicated:
+            reset_option(option_name, default_value)
+            value = default_value
     """
-    CustomOption.objects.filter(name=name).delete()
-    option = CustomOption(name=name, value=value)
+    CustomOption.objects.filter(name=name, user=user).delete()
+    option = CustomOption(name=name, value=value, user=user)
     option.save()
     return option
 
-def get_option(name):
+def get_option(name, default=None, user=None):
     """Returns value of an option
     """
     try:
-        option = CustomOption.objects.get(name=name)
+        option = CustomOption.objects.get(name=name, user=user)
     except CustomOption.DoesNotExist:
-        raise UnknownOption(_("Requested option '%s' does not exist") % name)
+        if default:
+            option = set_option(name, default, user)
+        else:
+            raise UnknownOption(_("Requested option '%s' does not exist") % name)
     except CustomOption.MultipleObjectsReturned:
         raise OptionDuplicated(_("There are more than one option objects. "
                                  "You can fix this problem by using "
                                  "'reset_option' function."))
     return option.value
+
+def get_user_option(name, user, default=None):
+    return get_option(name, default, user)
+
+def get_global_option(name, default=None):
+    return get_option(name, default)
+
+def set_user_option(name, value, user):
+    return set_option(name, value, user)
+
+def set_global_option(name, value):
+    return set_option(name, value)
+
+def get_options(list_of_names, list_of_defaults=None, user=None):
+    """Returns dictionary with values for all options from the list of names
+    """
+    values = {}
+    if not list_of_defaults:
+        list_of_defaults = [None for e in list_of_names]
+    for name, default in zip(list_of_names, list_of_defaults):
+        values[name] = get_option(name, default, user)
+    return values
+
+def options_form(options_dict):
+    """
+    Returns form class generated based on options dictionary
+    of plugin or widget.
+    """
+    class OptionsForm(forms.Form):
+        def __init__(self, *args, **kwargs):
+            super(OptionsForm, self).__init__(*args, **kwargs)
+            
+            for key in options_dict:
+                option = options_dict[key]
+                label = option.get('label', key)
+                option_type = option.get('type')
+                
+                if option.get('choices'):
+                    field = forms.ChoiceField(choices=option.get('choices'))
+                elif option_type == 'integer':
+                    field = forms.IntegerField()
+                elif option_type == 'bool':
+                    field = forms.BooleanField()
+                else:
+                    field = forms.CharField(max_length=CUSTOM_OPTION_MAX_LENGTH)
+                    
+                field.label = label   
+                self.fields[key] = field
+                
+    return OptionsForm
