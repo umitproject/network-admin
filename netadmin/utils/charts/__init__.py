3778f0eaf747f928c8908b6444e8dafb6da7aedc
piotrek <wasilewski.piotrek@gmail.com>
2011-08-21 17:08:11 +0200

    New imports in utils/charts/__init__.py

--- netadmin/utils/charts/__init__.py
+++ netadmin/utils/charts/__init__.py
@@ -18,7 +18,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from core import Chart
-from chart_tools import LineChart, ColumnChart, ScatterChart, \
+from charttools import LineChart, ColumnChart, ScatterChart, \
     AnnotatedTimeLine, PieChart, NumberColumn, StringColumn, \
     DateColumn, DatetimeColumn
