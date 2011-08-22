131b702c27d05ab1cdfc5ebf9cdf1f6145d91f23
piotrek <wasilewski.piotrek@gmail.com>
2011-08-22 04:26:51 +0200

    Removed unnecessary datetime import

--- netadmin/permissions/tests.py
+++ netadmin/permissions/tests.py
@@ -18,8 +18,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import datetime
-
 from django.contrib.auth.models import User
 from django.test import TestCase
 
