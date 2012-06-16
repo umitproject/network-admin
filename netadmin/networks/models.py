efa7fc8d5ee3f7a66cf7970230bb8a70123f55ea
Amit Pal <amix.pal@gmail.com>
2012-06-16 15:16:54 +0530

    Remove unique from Host name for unit test

--- netadmin/networks/models.py
+++ netadmin/networks/models.py
@@ -35,7 +35,7 @@ class NetworkObject(models.Model, SharedObject):
     Abstract model class for objects like host or network.
     Every object belongs to specified user
     """
-    name = models.CharField(max_length=250, unique=True)
+    name = models.CharField(max_length=250)
     description = models.TextField(blank=True)
     user = models.ForeignKey(User, blank=False, null=False)
     subnet = models.BooleanField(default= False)
@@ -72,7 +72,7 @@ class Host(NetworkObject):
     ipv4 = models.CharField(max_length=39,verbose_name=_("IPv4 address"),
                             validators=[IPv4_validation])
     ipv6 = models.CharField(max_length=39, verbose_name=_("IPv6 address"), 
-                            blank=True, null=True, validators=[IPv6_validation])
+                            blank=True, validators=[IPv6_validation])
 
     @permalink
     def get_absolute_url(self):
