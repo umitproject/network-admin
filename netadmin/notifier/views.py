24b47a9a89d34cc57818d7e7119441ff4c6d86e6
Amit Pal <amix.pal@gmail.com>
2012-07-17 03:06:57 +0530

    Added different notify sending method

--- netadmin/notifier/views.py
+++ netadmin/notifier/views.py
@@ -3,7 +3,7 @@
 
 # Copyright (C) 2011 Adriano Monteiro Marques
 #
-# Author: Piotrek Wasilewski <wasilewski.piotrek@gmail.com>
+# Author: Amit Pal <amix.pal@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -19,18 +19,44 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.http import HttpResponse
+from Skype4Py import Skype
+import sys
 
-from netadmin.notifier.utils import Notifier
 
+from netadmin.events.models import EventType, AlertCount
+from netadmin.shortcuts import get_alert_events, get_notifier
+
+def send_notification(request):
+	high, low, medium = ([] for i in range(3))
+	low,medium, high = ([get_alert_events(x) for x in (1,2,3)])
+	alert_count = AlertCount.objects.get(user=request.user.username)
+	l_al,m_al,h_al = alert_count.low, alert_count.medium, alert_count.high
+	if len(low)>=l_al:
+		low_notifier = get_notifier(request.user.username).low_notify
+		notify(request, low_notifier)
+	elif len(medium)>=m_al:
+		medium_notifier = get_notifier(request.user.username).medium_notify
+		notify(request, medium_notifier)
+	elif len(high)>=h_al:
+		high_notifier = get_notifier(request.user.username).high_notify
+		notify(request, high_notifier)
+	return HttpResponse()
+	
+def notify(request,notify_type):
+	if notify_type == 0:
+		send_emails(request)
+	elif notify_type == 1:
+		send_skype(request)
+	elif notify_type == 2:
+		send_irc(request)
 
 def send_emails(request):
-    notifier = Notifier()
-    try:
-        log = notifier.send_emails(clear_queue=True)
-    except NotifierEmptyQueue:
-        log = []
-    if log:
-        response = "<p>Emails sent:</p>%s" % '<br />'.join(log)
-    else:
-        response = "<p>No emails to send</p>"
-    return HttpResponse(response)
+	return
+
+def send_skype(request):
+	return
+
+def send_irc(request):
+	return
+	
+	
