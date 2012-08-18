2da96d3d6bb8f32586a07d0377b5a88d8295bf7f
amix <amix.pal@gmail.com>
2012-08-19 00:31:47 +0530

    Add options for plugin

--- installed_plugins/commands_exec/main.py
+++ installed_plugins/commands_exec/main.py
@@ -19,10 +19,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.utils.translation import ugettext as _
-
+from netadmin.shortcuts import get_hosts, get_commands
 from netadmin.plugins import Plugin, Widget
-from netadmin.shortcuts import get_user_events, get_hosts, get_host, \
-    get_eventtypes, get_events
+from netadmin.networks.models import HostCommand
 
 class CommandExecWidget(Widget):
     name = _("command execution")
@@ -31,74 +30,25 @@ class CommandExecWidget(Widget):
     username = []
 		
     def get_title(self, widget):
-		title = self.get_option('execute_the_commands_on_remote_Host', widget)
+		title = self.get_option('latest_events_widget_title', widget)
 		return title
     
     def options(self, widget):
-        hosts = get_hosts(self.user)
-        hosts_choices = [(-1, '-- all --')] + \
-            [(h.pk, h.name) for h in hosts]
-            
-        alert_levels = [(-1, _('-- any --')), (1, _('Low')),
-                        (2, _('Medium')), (3, _('High'))]
         return {
-            'latest_events_widget_number_of_events': {
-                'label': _("Number of events to show"),
+            'number_of_commands': {
+                'label': _("Number of command to show"),
                 'choices': [(i, i) for i in xrange(1, 16)],
                 'default': 5
-            },
-            'latest_events_widget_title': {
-                'label': _("Title of the widget"),
-                'default': _("Latest events"),
-                'return_func': lambda value: str(value)
-            },
-            'latest_events_widget_host': {
-                'label': _("Events from host"),
-                'choices': hosts_choices,
-                'default': -1,
-                'return_func': lambda value: None if value == '-1' else get_host(value)
-            },
-            'latest_events_widget_alert_level': {
-                'label': _("Alert level"),
-                'choices': alert_levels,
-                'default': -1,
-                'return_func': lambda value: int(value)
-            },
-            'latest_events_widget_checked': {
-                'label': _("Hide events marked as checked"),
-                'choices': [(0, 'No'), (1, 'Yes')],
-                'default': 0,
-                'return_func': lambda value: True if value == '1' else False
             }
         }
         
-    def context(self, widget):
-        host = self.get_option('latest_events_widget_host', widget)
-        alert_level = self.get_option('latest_events_widget_alert_level', widget)
-        
-        if host:
-            if alert_level > 0:
-                event_types = get_eventtypes(self.user, alert=alert_level)
-                events = get_events(source_hosts=[host],
-                                    event_types=event_types)
-            else:
-                events = host.events()
-        else:
-            if alert_level > 0:
-                event_types = get_eventtypes(self.user, alert=alert_level)
-                events = get_events(event_types=event_types)
-            else:
-                events = get_user_events(self.user)
-        
-        checked = self.get_option('latest_events_widget_checked', widget)
-        if checked:
-            events = events.filter(checked=False)
-        
-        limit = self.get_option('latest_events_widget_number_of_events', widget)
-        return {
-            'events': events.order_by('-timestamp')[:limit]
-        }
-
+	def context(self, widget):
+		command_obj = get_commands(user=self.user)
+		limit = self.get_option('number_of_commands', widget)
+		return {
+			'command_obj': command_obj[:limit]
+		}
+		
 class CommandExec(Plugin):
     name = _("commands execution")
     description = _("Execute the commands on remote host")
